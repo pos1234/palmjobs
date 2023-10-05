18653d11ac4c8c95ba544e78773168d6ac182edb
pos1234 <paulosegebeyehu5@gmail.com>
2023-10-06 00:54:38 +0700

    configuring routing for the pages

--- components/employerComponents/Jobs.tsx
+++ components/employerComponents/Jobs.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -7,29 +8,17 @@ import PinDropOutlinedIcon from '@mui/icons-material/PinDropOutlined';
 import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import Groups2OutlinedIcon from '@mui/icons-material/Groups2Outlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import { Popover } from '@headlessui/react';
 
-const Cards = (props: any) => {
-    return (
-        <div className="flex  justify-around bg-textW rounded-2xl shadow py-5 items-center w-[40%] md:w-[30%] lg:w-[20%] xl:w-[25%]">
-            <div className="flex flex-col justify-between">
-                <p className="font-[800] text-2xl text-[#000]">07</p>
-                <p className="opacity-70 text-slate-950 text-[14px] capitalize">Posted Job</p>
-            </div>
-            <div className="bg-skillColor text-gradientFirst flex items-center p-[0.6rem] jutify-center rounded-full">
-                <PersonIcon />
-            </div>
-        </div>
-    );
-};
 const PJobs = (props: any) => {
     return (
-        <div className="flex gap-x-4 border-b-[1px] py-4">
-            <div className="text-gradientFirst">
-                <BusinessCenterIcon />
-            </div>
-            <div className="col-span-10">
+        <div className="bg-textW grid grid-cols-12 relative py-5 pl-2 xl:pl-9">
+            <div className=" flex flex-col justify-center col-span-10 sm:col-span-7 lg:col-span-3">
                 <p className="text-neutral-900 text-lg font-medium leading-normal">Software Engineer</p>
-                <div className="flex flex-wrap text-stone-400 gap-5 mt-1">
+                <div className="flex flex-wrap text-stone-400 text-[0.8rem] gap-x-4 gap-y-1 mt-1">
                     <div>
                         <PinDropOutlinedIcon className="text-[1.1rem] -mt-1" /> <span>Addis Abeba</span>
                     </div>
@@ -39,64 +28,114 @@ const PJobs = (props: any) => {
                     <div>
                         <AttachMoneyOutlinedIcon className="text-[1.1rem] -mt-1" /> <span>50 - 55k</span>
                     </div>
-                    <div>
-                        <CalendarTodayOutlinedIcon className="text-[1.1rem] -mt-1" /> <span>20 min ago</span>
+                    <div className="flex items-center flex gap-x-2 lg:hidden ">
+                        <CalendarTodayOutlinedIcon className="text-[1.1rem] text-gradientFirst -mt-1" /> <span>20-70-2023</span>
+                    </div>
+                    <div className="flex items-center flex gap-x-2 lg:hidden">
+                        <Groups2OutlinedIcon className="text-[1.1rem] text-gradientFirst -mt-1" /> <span>120 Applicants</span>
                     </div>
                 </div>
+            </div>
+            <div className="col-span-2 flex items-center flex gap-x-2 lg:text-[0.9rem] hidden  lg:flex">
+                <CalendarTodayOutlinedIcon className="text-[1.1rem] text-gradientFirst -mt-0.5" /> <span>20-70-2023</span>
+            </div>
+            <div className="col-span-3 flex items-center flex gap-x-2 lg:text-[0.9rem] hidden lg:flex">
+                <Groups2OutlinedIcon className="text-[1.1rem] text-gradientFirst -mt-0.5" /> <span>120 Applicants</span>
+            </div>
+            <div className="col-span-2 flex items-center max-sm:hidden lg:text-[0.9rem]">
+                <select className="bg-lightGreen text-green border-0 rounded-2xl">
+                    <option>Active</option>
+                </select>
+            </div>
+            <div className="flex items-center pl-0 sm:pl-2 col-span-2  sm:col-span-3 lg:col-span-2 lg:text-[0.9rem] ">
+                <Popover className="w-full sm:relative focus:ring-0 focus:border-0 focus:outline-0">
+                    <Popover.Button className="focus:ring-0 focus:border-0 focus:outline-0">
+                        <MoreHorizIcon className="text-[2.5rem] focus:ring-0 focus:border-0 focus:outline-0 -mt-1 cursor-pointer" />
+                    </Popover.Button>
+
+                    <Popover.Panel className="absolute -ml-28 sm:ml-0 w-[10rem] sm:w-full border-2 rounded-2xl flex w-full flex-col gap-y-3 bg-textW py-3 px-3 bg-white shadow z-10">
+                        <div className="flex gap-x-3 text-[0.8rem] md:max-lg:text-red-500 cursor-pointer items-center text-stone-400 hover:text-stone-700">
+                            <BorderColorIcon className="text-[1rem]" />
+                            <span>Edit Job</span>
+                        </div>
+                        <div className="flex gap-x-3 text-[0.8rem] cursor-pointer items-center text-stone-400 hover:text-stone-700">
+                            <VisibilityIcon className="text-[1rem]" />
+                            <span>View Details</span>
+                        </div>
+                        <div className="flex gap-x-3 text-[0.8rem] cursor-pointer items-center text-stone-400 hover:text-stone-700">
+                            <ShareOutlinedIcon className="text-[1rem]" />
+                            <span>Share</span>
+                        </div>
+                    </Popover.Panel>
+                </Popover>
             </div>
         </div>
     );
 };
+const JobTab = (props: any) => {
+    return (
+        <div
+            className={
+                props.active == 'true'
+                    ? 'text-textW bg-gradient-to-r flex items-center from-gradientFirst to-gradientSecond justify-center cursor-pointer h-16 rounded-2xl px-5'
+                    : 'text-stone-500 flex items-center justify-center cursor-pointer h-16 rounded-2xl px-5'
+            }
+        >
+            {props.text}
+        </div>
+    );
+};
+
 const Jobs = () => {
     return (
-        <div className="bg-textW">
-            <div className="bg-green-500 relative  pl-20 pt-10 lg:h-60 xl:h-48 jobsBack">
-                <p className="text-black text-3xl font-[700]">Dashboard</p>
+        <div className="bg-textW min-h-screen">
+            <div className="relative flex justify-between pt-10 items-center lg:pl-10">
+                <p className="text-black text-3xl font-[700]">Jobs</p>
+                <div className="text-textW bg-gradient-to-r flex items-center from-gradientFirst to-gradientSecond justify-center cursor-pointer h-16 rounded-xl px-9">
+                    <BorderColorIcon sx={{ fontSize: '1.2rem' }} className="mr-2" /> Post Job
+                </div>
             </div>
-            <div className=" pt-5 pl-10 pb-10 bg-textW min-h-screen lg:-mt-40 xl:-mt-24 xl:pr-10 xl:px-20">
-                <div className="flex justify-around max-xl:flex-wrap bg-forBack gap-5 pt-5 w-[100%] lg:justify-start lg:justify-around">
-                    <div className="flex  justify-around bg-textW rounded-2xl shadow py-8 items-center w-[40%]  lg:w-[20%] xl:w-[25%]">
-                        <div className="flex flex-col justify-between">
-                            <p className="font-[800] text-2xl text-[#000]">07</p>
-                            <p className="opacity-70 text-slate-950 text-[14px] capitalize">Posted Job</p>
-                        </div>
-                        <div className="bg-skillColor text-gradientFirst flex items-center p-[0.8rem] jutify-center rounded-full">
-                            <PersonIcon sx={{fontSize: '1.2rem'}}/>
+            <div className="mt-8 lg:pl-10">
+                <div className="flex max-sm:flex-col gap-y-3 justify-between">
+                    <div className="bg-forBack py-2 px-1">
+                        <div className="flex bg-textW">
+                            <JobTab text="Opened" active="true" />
+                            <JobTab text="Paused" />
+                            <JobTab text="Closed" />
                         </div>
                     </div>
-                    <div className="flex  justify-around bg-textW rounded-2xl shadow py-8 items-center w-[40%]  lg:w-[20%] xl:w-[25%]">
-                        <div className="flex flex-col justify-between">
-                            <p className="font-[800] text-2xl text-[#000]">03</p>
-                            <p className="opacity-70 text-slate-950 text-[14px] capitalize">Shortlisted</p>
-                        </div>
-                        <div className="bg-skillColor text-gradientFirst flex items-center p-[0.8rem] jutify-center rounded-full">
-                            <BookmarkIcon sx={{fontSize: '1.2rem'}}/>
-                        </div>
-                    </div>
-                    <div className="flex  justify-around bg-textW rounded-2xl shadow py-8 items-center w-[40%] lg:w-[20%] xl:w-[25%]">
-                        <div className="flex flex-col justify-between">
-                            <p className="font-[800] text-2xl text-[#000]">1.7k</p>
-                            <p className="opacity-70 text-slate-950 text-[14px] capitalize">Application</p>
-                        </div>
-                        <div className="bg-skillColor text-gradientFirst flex items-center p-[0.8rem] jutify-center rounded-full">
-                            <VisibilityIcon sx={{fontSize: '1.2rem'}}/>
-                        </div>
-                    </div>
-                    <div className="flex  justify-around bg-textW rounded-2xl shadow py-8 items-center w-[40%] lg:w-[20%] xl:w-[25%]">
-                        <div className="flex flex-col justify-between">
-                            <p className="font-[800] text-2xl text-[#000]">04</p>
-                            <p className="opacity-70 text-slate-950 text-[14px] capitalize">Hired</p>
-                        </div>
-                        <div className="bg-skillColor text-gradientFirst flex items-center p-[0.8rem] jutify-center rounded-full">
-                            <BorderColorIcon sx={{fontSize: '1.2rem'}}/>
+                    <div className="flex max-sm:pl-5 items-center gap-x-2">
+                        <p>Sort By:</p>
+                        <div className="bg-forBack p-1">
+                            <select className="rounded-2xl py-3 border-0 cursor-pointer">
+                                <option>Posting Date</option>
+                            </select>
                         </div>
                     </div>
                 </div>
-
-                <div className="bg-forBack mt-10 px-2 py-3">
-                    <div className="bg-textW pt-5">
-                        <p className="text-black text-3xl font-[700]">Posted Jobs</p>
-                        <div className="max-h-[25rem] overflow-y-auto">
+                <div>
+                    <div className="grid grid-cols-12 pt-3">
+                        <div className="col-span-12  grid grid-cols-12 max-md:hidden xl:pl-10">
+                            <p className="col-span-3 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 md:max-lg:hidden max-xl:text-[1rem]">
+                                Job Title
+                            </p>
+                            <p className="col-span-7 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 hidden md:max-lg:block">
+                                Job Detail
+                            </p>
+                            <p className="col-span-3 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 text-[1rem] md:max-lg:hidden max-xl:text-[1rem] lg:col-span-2">
+                                Job Created
+                            </p>
+                            <p className="col-span-3 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 max-xl:text-[1rem] md:max-lg:hidden">
+                                Job Applicants
+                            </p>
+                            <p className="col-span-2 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 md:max-lg:col-span-2 lg:max-xl:text-[1rem]">
+                                Status
+                            </p>
+                            <p className="col-span-1 text-neutral-900 text-opacity-70 text-lg px-2 font-medium leading-loose py-2 md:max-lg:col-span-3 lg:max-xl:text-[1rem]">
+                                Actions
+                            </p>
+                        </div>
+                        <div className="col-span-12 space-y-4 bg-forBack  h-[25rem] overflow-y-auto p-1 xl:p-3">
                             <PJobs />
                             <PJobs />
                             <PJobs />
@@ -107,4 +146,5 @@ const Jobs = () => {
         </div>
     );
 };
+
 export default Jobs;
---
+++ pages/index2.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import { /* accountData, */ getUserData, fetchJobs, applyToJobs, alreadyApplied, saveJobs, alreadySaved } from '../lib/services';
+import { PostedJob } from '@/lib/Interfaces';
+import { useUser } from '@/lib/context';
+import { useRouter } from 'next/router';
+const JobDetails = (props: any) => {
+    const { loading, user, role } = useUser();
+    const [checkUser, setCheckUser] = useState(false);
+    const router = useRouter();
+    useEffect(() => {
+        try {
+            const cand = !(role == '' || role == 'candidate') ? true : false;
+            if ((!user && !loading) || cand) {
+                setCheckUser(true);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, [user, loading, role]);
+
+    const handleApply = (jobId: string, employerId: string) => {
+        if (checkUser) {
+            router.push('/account/signIn');
+        } else {
+            router.push({
+                pathname: '/users/candidate/applyToJob',
+                query: { param1: jobId, param2: employerId }
+            });
+        }
+    };
+    const handleSaveJob = (jobId: string) => {
+        if (checkUser) {
+            router.push('/account/signIn');
+            console.log('hey');
+            
+        } else {
+            const checkJob = alreadyApplied(props.userId, jobId);
+            checkJob.then((res) => {
+                const checkSaved = alreadySaved(props.userId, jobId);
+                /* console.log(res); */
+                checkSaved.then((rep)=>console.log(rep))
+                    console.log(checkSaved);
+                    
+                /* if (res.total == 0) { */
+                    checkSaved.then((rem: any) => {
+                        console.log('saved message' + rem);
+                        
+                        if (rem.total == 0) {
+                            console.log('not saved');
+                            saveJobs(props.userId, jobId);
+                        } else {
+                            console.log('already saved this job');
+                        }
+                    });
+                //}
+            });
+            /* router.push({
+                pathname: '/users/candidate/applyToJob',
+                query: { param1: jobId, param2: employerId }
+            }); */
+        }
+    };
+    return (
+        <tr key={props.$id}>
+            <td style={{ border: '1px solid green' }}>{props.companyName}</td>
+            <td style={{ border: '1px solid green' }}>{props.position}</td>
+            <td style={{ border: '1px solid green' }}>{props.location}</td>
+            <td style={{ border: '1px solid green' }}>{props.industry}</td>
+            <td style={{ border: '1px solid green' }}>{props.gender}</td>
+            <td style={{ border: '1px solid green' }}>{props.skills}</td>
+            <td style={{ border: '1px solid green' }}>{props.salary}</td>
+            <td style={{ border: '1px solid green' }}>{props.stat}</td>
+            <td>
+                {props.externalLink ? (
+                    <a href={props.externalLink}>link</a>
+                ) : (
+                    <button onClick={() => handleApply(props.jobId, props.employerId)}>Apply</button>
+                )}
+            </td>
+            <td>
+                <button onClick={() => handleSaveJob(props.jobId)}>save</button>
+            </td>
+        </tr>
+    );
+};
+const Jobs = () => {
+    const [userData, setUserData] = useState<any>();
+    /*     const userData: any = accountData;
+     */ useEffect(() => {
+        try {
+            const userInfo: any = getUserData();
+            userInfo.then((res: any) => setUserData(res));
+        } catch (e) {
+            console.log(e);
+        }
+    }, []);
+    const jobs = fetchJobs();
+
+    return (
+        <>
+            <h1 className="text-red-500 font-bigW text-bigS leading-bigL">This is the jobs page</h1>
+            <p>hey</p>
+            {userData && (
+                <div>
+                    <h1>{userData.name}</h1>
+                    <h1>{userData.email}</h1>
+                </div>
+            )}
+            <table style={{ marginLeft: '15%', marginTop: '5%', width: '80vw' }} border={1}>
+                <tbody>
+                    {jobs &&
+                        jobs.map((datas: PostedJob) => {
+                            return (
+                                <JobDetails
+                                    userId={userData && userData.$id}
+                                    companyName={datas.companyName}
+                                    position={datas.jobTitle}
+                                    location={datas.jobLocation}
+                                    industry={datas.jobIndustry}
+                                    gender={datas.prefferedGender}
+                                    skills={datas.requiredSkills[0]}
+                                    salary={datas.salaryRange}
+                                    stat={datas.jobStatus}
+                                    jobId={datas.$id}
+                                    externalLink={datas.externalLink}
+                                    key={datas.$id}
+                                    employerId={datas.employerId}
+                                />
+                            );
+                        })}
+                </tbody>
+            </table>
+        </>
+    );
+};
+export default Jobs;
